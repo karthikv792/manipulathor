db136e789d800859c40b2dc92a88c54da3275af5
roseh-ai2 <roseh@allenai.org>
2022-05-09 17:53:57 -0700

    procthor stretch objectnav task, task sampler, and starter configs

--- utils/procthor_utils/procthor_helper.py
+++ utils/procthor_utils/procthor_helper.py
@@ -1,5 +1,13 @@
+from typing import Dict, List, Literal, Optional, TypedDict
+import math
+
 from manipulathor_utils.debugger_util import ForkedPdb
 from omegaconf import DictConfig, OmegaConf
+from ai2thor.controller import Controller
+from ai2thor.util import metrics
+from allenact.utils.cache_utils import DynamicDistanceCache
+from allenact.utils.system import get_logger
+from utils.procthor_utils.procthor_types import Vector3
 
 
 # def get_reachable_positions_procthor(controller):
@@ -8,3 +16,104 @@ from omegaconf import DictConfig, OmegaConf
 #     reachable_positions = event.metadata['actionReturn']
 #     return reachable_positions
 
+
+def position_dist(
+    p0: Vector3,
+    p1: Vector3,
+    ignore_y: bool = False,
+    dist_fn: Literal["l1", "l2"] = "l2",
+) -> float:
+    """Distance between two points of the form {"x": x, "y": y, "z": z}."""
+    if dist_fn == "l1":
+        return (
+            abs(p0["x"] - p1["x"])
+            + (0 if ignore_y else abs(p0["y"] - p1["y"]))
+            + abs(p0["z"] - p1["z"])
+        )
+    elif dist_fn == "l2":
+        return math.sqrt(
+            (p0["x"] - p1["x"]) ** 2
+            + (0 if ignore_y else (p0["y"] - p1["y"]) ** 2)
+            + (p0["z"] - p1["z"]) ** 2
+        )
+    else:
+        raise NotImplementedError(
+            'dist_fn must be in {"l1", "l2"}.' f" You gave {dist_fn}"
+        )
+
+
+def distance_to_object_id(
+    controller: Controller,
+    distance_cache: DynamicDistanceCache,
+    object_id: str,
+    house_name: str,
+) -> Optional[float]:
+    """Minimal geodesic distance to object of given objectId from agent's
+    current location.
+    It might return -1.0 for unreachable targets.
+    # TODO: return None for unreachable targets.
+    """
+
+    def path_from_point_to_object_id(
+        point: Dict[str, float], object_id: str, allowed_error: float
+    ) -> Optional[List[Dict[str, float]]]:
+        event = controller.step(
+            action="GetShortestPath",
+            objectId=object_id,
+            position=point,
+            allowedError=allowed_error,
+        )
+        if event:
+            return event.metadata["actionReturn"]["corners"]
+        else:
+            get_logger().debug(
+                f"Failed to find path for {object_id} in {house_name}."
+                f' Start point {point}, agent state {event.metadata["agent"]}.'
+            )
+            return None
+
+    def distance_from_point_to_object_id(
+        point: Dict[str, float], object_id: str, allowed_error: float
+    ) -> float:
+        """Minimal geodesic distance from a point to an object of the given
+        type.
+        It might return -1.0 for unreachable targets.
+        """
+        path = path_from_point_to_object_id(point, object_id, allowed_error)
+        if path:
+            # Because `allowed_error != 0` means that the path returned above might not start
+            # at `point`, we explicitly add any offset there is.
+            dist = position_dist(p0=point, p1=path[0], ignore_y=True)
+            return metrics.path_distance(path) + dist
+        return -1.0
+
+    def retry_dist(position: Dict[str, float], object_id: str) -> float:
+        allowed_error = 0.05
+        debug_log = ""
+        d = -1.0
+        while allowed_error < 2.5:
+            d = distance_from_point_to_object_id(position, object_id, allowed_error)
+            if d < 0:
+                debug_log = (
+                    f"In house {house_name}, could not find a path from {position} to {object_id} with"
+                    f" {allowed_error} error tolerance. Increasing this tolerance to"
+                    f" {2 * allowed_error} any trying again."
+                )
+                allowed_error *= 2
+            else:
+                break
+        if d < 0:
+            get_logger().warning(
+                f"In house {house_name}, could not find a path from {position} to {object_id}"
+                f" with {allowed_error} error tolerance. Returning a distance of -1."
+            )
+        elif debug_log != "":
+            get_logger().debug(debug_log)
+        return d
+
+    return distance_cache.find_distance(
+        scene_name=house_name,
+        position=controller.last_event.metadata["agent"]["position"],
+        target=object_id,
+        native_distance_function=retry_dist,
+    )
